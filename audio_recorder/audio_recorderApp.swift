2cce5333c839a255ba23baa2b45f71b106fa32c5
Anastasiya <masnas2004@gmail.com>
2025-01-15 20:27:17 -0500

    Initial Commit

---
+++ audio_recorder/audio_recorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  audio_recorderApp.swift
+//  audio_recorder
+//
+//  Created by Anastasiya Masalava on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct audio_recorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
